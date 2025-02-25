9a72f38a8dea1bb8a4c35b3adc429bd72c62ca5a
YuryLandau <yurylandau@gmail.com>
2025-02-25 01:36:51 -0300

    feat: estilizar home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,26 +1,24 @@
-import { Earth } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import city_list from "../constants/city_list";
 
 export default function Home() {
   return (
 
-    <main className="mx-auto px-5 py-8 bg-gray-900 text-beje-400">
-      <div className="min-h-dvh flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center w-full ">
-          <h2 className="text-4xl text-beje-200 mb-4 text-center">Nimbus Weather</h2>
-          <Earth className="size-44" />
+    <main className="min-h-dvh mx-auto px-5 flex flex-col justify-start md:justify-center bg-gray-600 text-gray-100">
+      <div className="flex flex-col items-center py-8 w-full">
+        <h1 className="text-5xl font-light text-center">Weather</h1>
 
-          <div className="flex items-center justify-center gap-2 pt-6 flex-wrap text-beje-200 ">
-            {city_list.map((city) => (
-              <div key={city.id} className="flex items-center gap-2 hover:text-beje-400 border-b-2 hover:border-b-beje-400 ">
-                <Link className="transition-all duration-300 hover:-translate-y-1" href={`/city/${city.name}`}>
-                  <span className="">{city.name}</span>
-                </Link>
-              </div>
-            ))}
-          </div>
+        <h2 className="text-2xl font-extralight text-center">Select a city</h2>
 
+        <Image className="dark:invert py-6" src={`/assets/logo.svg`} alt="Logo" width={176} height={176} />
+
+        <div className="grid grid-cols-3 grid-rows-2 gap-6">
+          {city_list.map((city) => (
+            <Link className="text-center min-w-[90px]" key={city.id} href={`/city/${city.name}`}>
+              {city.name}
+            </Link>
+          ))}
         </div>
       </div>
     </main>
