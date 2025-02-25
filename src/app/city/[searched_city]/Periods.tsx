ab75a88d3f11be02ffcf21d14ff76d0a6292af9b
YuryLandau <yurylandau@gmail.com>
2025-02-24 23:30:35 -0300

    feat: adaptar momentos ao style guide

--- src/app/city/[searched_city]/Periods.tsx
+++ src/app/city/[searched_city]/Periods.tsx
@@ -15,7 +15,7 @@ const PeriodRoot = ({ header, temperature, children, ...props }: PeriodRootProps
                 ? "Afternoon" : "Night"
 
     return (
-        <div className="flex flex-col items-center justify-center gap-1 " {...props}>
+        <div className="flex flex-col items-center justify-center gap-4 " {...props}>
             <span className="text-xl">
                 {periodHeader}
             </span>
@@ -37,19 +37,19 @@ const Periods = (props: PeriodProps) => {
     const { forecastList } = props;
 
     return (
-        <div className="grid gap-4 md:grid-cols-5 pt-2">
+        <div className="grid gap-12 md:grid-cols-4">
 
             {
                 forecastList.filter((forecast) => forecast.dt_txt.includes("03:00:00")
                     || forecast.dt_txt.includes("09:00:00")
                     || forecast.dt_txt.includes("15:00:00")
-                    || forecast.dt_txt.includes("21:00:00")).slice(0, 5).map((forecast) => (
+                    || forecast.dt_txt.includes("21:00:00")).slice(0, 4).map((forecast) => (
                         <PeriodRoot
                             key={forecast.dt}
                             header={forecast.dt_txt}
                             temperature={forecast.main.temp.toFixed(0)}>
 
-                            <Image src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`} alt={forecast.weather[0].description} width={100} height={100} />
+                            <Image src={`/assets/icons/${forecast.weather[0].icon}.svg`} alt={forecast.weather[0].description} width={48} height={48} />
                         </PeriodRoot>
                     ))
             }
